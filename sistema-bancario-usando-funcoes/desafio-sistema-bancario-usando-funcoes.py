8705cf89839f22d9a3be9825706300cf917114c5
joao-nzuzi <joaonzuzi21@gmail.com>
2024-06-10 00:17:10 +0100

    feat: criação da função levantar / sacar

--- sistema-bancario-usando-funcoes/desafio-sistema-bancario-usando-funcoes.py
+++ sistema-bancario-usando-funcoes/desafio-sistema-bancario-usando-funcoes.py
@@ -27,6 +27,31 @@ def depositar(saldo, extrato, valor, /):
         
     return saldo, extrato
 
+def levantar(*, saldo, extrato, limite_levantamento_diario, numero_levantamento_diario, numero_levantamento_feitos):
+    
+    if numero_levantamento_feitos >= 0 and numero_levantamento_feitos <= numero_levantamento_diario - 1:
+        
+        valor = float(input('Informa o valor que deseja levantar: '))
+        
+        if valor <= saldo:
+            if valor >= 0:
+                if valor <= limite_levantamento_diario:
+                    saldo -= valor
+                    extrato += f"Foi levantado: R$ {valor:.2f} aos {datetime.datetime.now(pytz.timezone('Africa/Luanda'))}\n"
+                    numero_levantamento_feitos += 1
+                    print("Depósito feito com sucesso")
+                else:
+                    print(f"Excedeu o limite do valor máximo permitido por levantamento. Só é permitido levantar R$ {limite_levantamento_diario:.2f} por vez.")
+            else:
+                print("É apenas permitido levantamento de valor positivo. Por favor, informa um valor positivo.")
+        else:
+            print("Não é possivel levantar o dinheiro por falta de saldo na conta.")
+    else:
+        print(f"Excedeu o nº de levantamento permitidos por dia. Só é permitido {numero_levantamento_diario} por dia")
+    
+    return saldo, extrato
+
+
 
 def executar():
     NUMERO_LEVANTAMENTO_DIARIO_PERMITIDO = 3
